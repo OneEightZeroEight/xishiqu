516ed53676842e2bf9f79450cc78e1f3a73e0610
cheng <929507896@qq.com>
2018-12-03 21:59:44 +0800

    上传部分背景图标以及Xcategory.jsx组件

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 import Xheader from '../../common/Xheader.jsx';
 import Xfooter from '../../common/Xfooter.jsx';
 
+import Xcotegory from '../../common/Xcategory.jsx';
+
 class Home extends Component {
 	render() {
 		return (
 			<div id="wrapper-home">
 				<div id="home" className="page">
 					<Xheader />
-
+					<div className="block-wrapper">
+						<Xcotegory />
+					</div>
 				</div>
 				<Xfooter />
 			</div>
