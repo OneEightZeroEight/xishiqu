f7c26d3320c9dc8a74dfa7a118c465cd0891a043
oufeiyue <1453467473@qq.com>
2018-12-03 16:34:37 +0800

    home文件修改

---
+++ src/components/pages/Home/Lunbo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Lunbo extends Component {
+  render() {
+    return (
+      <div className="Lunbo">
+        '这里是lunbo'
+      </div>
+    );
+  }
+}
+
+export default Lunbo;
